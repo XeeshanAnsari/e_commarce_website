f9967a214148505cfa5b047e19334d16ab6b96b6
xeeshan <xeeshan@gmail.com>
2016-11-07 07:20:36 -0800

    add navigation bar

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HOme</title>
+    <link rel="stylesheet" href="./styles/bootstrap.min.css">
+    <link rel="stylesheet" href="./styles/style.css">
+    <script src="./js/jquery.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div> 
+        <div class="container">
+            <div class="header">
+                
+            </div>
+            <!--Naviigation-->
+                <div class="navbar navbar-default ">
+                   <div class="navbar-brand">
+                       E-Commarce
+                    </div>
+                     
+                    <div class="navbar-header navbar-centre">
+                       <ul class=" nav navbar-nav ">
+                          <li><a href="#">Home</a></li>
+                          <li><a href="#">All Product</a></li>
+                          <li><a href="#">My Account</a></li>
+                          <li><a href="#">Sign Up</a></li>
+                          <li><a href="#">Shopping Cart</a></li>
+                           <li><a href="#">Contact Us</a></li> 
+                        </ul>
+                    </div>  
+                    <div class="navbar-right">
+                        <form method="post" action="result.php" class="navbar-form navbar-left" role="search">
+                            <div class="form-group">
+                                 <input type="text" class="form-control" name="user_query" placeholder="Search a Product">
+                             </div>
+                             <button type="submit" class="btn btn-default">Submit</button>
+                        </form>
+                    </div>          
+                  </div>
+            <!--Naviigation-->
+            
+            
+             <!--main Body-->
+             
+             <div class="container">
+                 <div class="row">
+                     <div class="col-md-3" id="left-side">gou</div>
+                     <div class="col-md-9">hpigp</div>
+                 </div>
+                 
+             </div>
+        </div>
+    </div>
+</body>
+</html>
